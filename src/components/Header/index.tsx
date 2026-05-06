d12b5386da5bbb0b311167f96158b07cb548e15c
SthivemMartins <sthivemm@gmail.com>
2026-05-06 07:28:18 -0300

    feat: criados os componentes de drawer e do botão de carrinho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,40 @@
 import Logo from "@/assets/images/logo.png";
 import IconUser from "@/assets/images/icon-user.png";
-import IconAbout from "@/assets/images/icon-about.png";
 import { Link } from "@tanstack/react-router";
-import { ShoppingCart } from "../ShoppingCart";
 import { MenuMobile } from "../MenuMobile";
+import { CartButton } from "../CartButton";
+import { CartDrawer } from "../CartDrawer";
+import { useState } from "react";
 
 export interface NavLink {
   name: string;
   href: string;
-};
+}
 
 const navLinks: NavLink[] = [
   { name: "Masculino", href: "/products/category/masculino" },
   { name: "Feminino", href: "/products/category/feminino" },
   { name: "Outlet", href: "/products/category/outlet" },
-]
+];
 
 export const Header = () => {
+
+  const [cartIsOpen, setCartIsOpen] = useState<boolean>(false);
+
   return (
     <div className="relative">
       <header className="fixed top-5 left-0 right-0 z-10 mx-10">
         <div className="bg-white text-black max-w-[1320px] mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
-
           <Link to="/">
             <img src={Logo} alt="Logo SyntaxWear" className="w-32 md:w-36" />
           </Link>
 
           <nav className="hidden lg:block">
-            <ul className="flex gap-10 items-center">
+            <ul className="flex gap-10">
               {navLinks.map((link) => (
-                <Link to={link.href}>{link.name}</Link>
+                <Link to={link.href} key={link.name}>
+                  {link.name}
+                </Link>
               ))}
             </ul>
           </nav>
@@ -50,14 +55,15 @@ export const Header = () => {
                   <img src={IconUser} alt="Ícone de login" />
                 </Link>
               </li>
-              
               <li>
-                <ShoppingCart />
+                <CartButton onClick={() => setCartIsOpen(true)}/>
               </li>
             </ul>
           </nav>
         </div>
       </header>
+
+      <CartDrawer isOpen={cartIsOpen} onClose={() => setCartIsOpen(false)} />
     </div>
   );
 };
